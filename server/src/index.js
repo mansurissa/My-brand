ec69beb0f9b003e4622080de09d0b156cb001649
mansurissa <mansurissa6@gmail.com>
2020-10-10 08:27:20 +0200

    setup connection to mongo db

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,20 @@
-import express from "express";
+import express, { json } from "express";
 import { config } from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import blogRouter from "./routes/blogRouter";
+import usersRouter from "./routes/usersRouter";
+import connectDb from "./config/database";
 
 config();
 const app = express();
+app.use(json());
+app.use(cors());
+app.use(morgan("dev"));
+connectDb();
+
+app.use("/superblog", blogRouter);
+app.use("/users", usersRouter);
 
 const port = process.env.PORT;
 app.listen(port, console.log("Server is running on port:", port));
