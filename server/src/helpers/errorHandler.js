50b123340717e0dbcbb31d9e276a77249276ea52
mansurissa <mansurissa6@gmail.com>
2020-10-12 15:45:55 +0200

    moving server to it own repo

--- server/src/helpers/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const errorRes = (res, status, message) => {
-  res.status(status).json({
-    success: false,
-    message,
-    data: {},
-  });
-};
-
-export default errorRes;
--- server/src/routes/usersRouter.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { login, register } from "../controllers/usersController";
-
-const usersRouter = express.Router();
-usersRouter.route("/register").post(register);
-usersRouter.route("/login").post(login);
-export default usersRouter;
